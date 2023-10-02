4905646ed8a612f5597eaa1effec230a4e5aeb20
povilastiskus <povilastiskus@gmail.com>
2023-10-02 15:27:36 +0300

    brands changes

--- povilo_servisas/autoservisas/views.py
+++ povilo_servisas/autoservisas/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from . import models
 
-all_brands = models.CarModel.objects.values_list("make", flat=True)
-unique_brands = set(all_brands)
+
 
 def index(request):
+    brands = models.CarModel.objects.values_list("make", flat=True).distinct()
     context = {
         "num_cars": models.Car.objects.count(),
         "num_makes": models.CarModel.objects.values("make").distinct().count(),
         "num_orders": models.OrderLine.objects.count(),
-        "brands": unique_brands,
+        "brands": brands,
     }
     return render(request, "library/index.html", context)
