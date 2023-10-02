fe3a0661132c95dd63cc09123f0a287ba6e8e623
povilastiskus <povilastiskus@gmail.com>
2023-10-02 15:07:09 +0300

    header 2 swap

--- povilo_servisas/autoservisas/views.py
+++ povilo_servisas/autoservisas/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
 from . import models
 
-def parts_and_services(request):
-    parts_and_services_list = models.PartService.objects.all()
-    return render(request, 'parts_and_services.html', {'partsandervices': parts_and_services_list})
+all_brands = models.CarModel.objects.values_list("make", flat=True)
+unique_brands = set(all_brands)
 
 def index(request):
     context = {
         "num_cars": models.Car.objects.count(),
-        "num_makes": models.CarModel.objects.values('make').distinct().count(),
+        "num_makes": models.CarModel.objects.values("make").distinct().count(),
         "num_orders": models.OrderLine.objects.count(),
-        "parts_services": models.PartService.objects.all(),
+        "brands": unique_brands,
     }
     return render(request, "library/index.html", context)
