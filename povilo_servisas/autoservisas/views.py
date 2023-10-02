c1b0547871c41873017204f2b9b32d6fa3e915a7
povilastiskus <povilastiskus@gmail.com>
2023-10-02 11:41:41 +0300

    views html intro page

--- povilo_servisas/autoservisas/views.py
+++ povilo_servisas/autoservisas/views.py
@@ -1,7 +1,15 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from . import models
 
-# Create your views here.
+def parts_and_services(request):
+    parts_and_services_list = models.PartService.objects.all()
+    return render(request, 'parts_and_services.html', {'partsandervices': parts_and_services_list})
 
 def index(request):
-    return HttpResponse("Autoservisas veikia")
+    context = {
+        "num_cars": models.Car.objects.count(),
+        "num_makes": models.CarModel.objects.values('make').distinct().count(),
+        "num_orders": models.OrderLine.objects.count(),
+        "parts_services":models.PartService.objects.all(),
+    }
+    return render(request, "library/index.html", context)
