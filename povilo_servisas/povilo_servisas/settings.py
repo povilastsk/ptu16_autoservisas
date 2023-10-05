01088e5b0a439d9256e0f31b71dcdc92e531bc1c
povilastiskus <povilastiskus@gmail.com>
2023-10-05 11:15:48 +0300

    password reset functionality

--- povilo_servisas/povilo_servisas/settings.py
+++ povilo_servisas/povilo_servisas/settings.py
@@ -125,3 +125,12 @@ STATIC_ROOT = BASE_DIR.joinpath(STATIC_URL)
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = local_settings.EMAIL_HOST
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = local_settings.EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD = local_settings.EMAIL_HOST_PASSWORD
