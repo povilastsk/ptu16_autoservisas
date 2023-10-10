36e0d1a1fd20f7710860bec810234119ee996cad
povilastiskus <povilastiskus@gmail.com>
2023-10-10 20:47:11 +0300

    image resizer

--- povilo_servisas/user_profile/models.py
+++ povilo_servisas/user_profile/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
 from django.urls import reverse
+from PIL import Image
 
 
 User = get_user_model()
@@ -20,6 +21,15 @@ class Profile(models.Model):
     class Meta:
         verbose_name = _("profile")
         verbose_name_plural = _("profiles")
+
+    def save(self, *args, **kwargs) -> None:
+        super().save(*args, **kwargs)
+        if self.photo:
+            photo = Image.open(self.photo.path)
+            if photo.height > 300 or photo.width > 500:
+                resized_dimensions = (500, 300)
+                photo.thumbnail(resized_dimensions)
+                photo.save(self.photo.path)
 
     def __str__(self):
         return f"{self.user}"
