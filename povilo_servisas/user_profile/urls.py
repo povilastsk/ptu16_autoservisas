50e0562d525bcb58d5dee8b43dc51293a4969f2e
povilastiskus <povilastiskus@gmail.com>
2023-10-10 20:45:55 +0300

    user profile update view

--- povilo_servisas/user_profile/urls.py
+++ povilo_servisas/user_profile/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('profile/', views.profile, name="profile"),
-    
+    path('profile/update/', views.profile_update, name='profile_update'),
 ]
